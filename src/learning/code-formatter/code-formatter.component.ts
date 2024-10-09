c18f606b05623d5f355e1728f642a1537a45bec5
code-fast-channel <codefastchannel@gmail.com>
2024-10-09 09:08:57 +0530

    changes

--- src/learning/code-formatter/code-formatter.component.ts
+++ src/learning/code-formatter/code-formatter.component.ts
@@ -1,16 +1,16 @@
 import { Component, ElementRef, Input, ViewChild } from '@angular/core';
-// import * as prettier from 'prettier';
 
 declare const ace: any;
+declare const prettier: any; // Declare the global prettier object
+declare const prettierPlugins: any; // Declare the plugins object from the CDN
 
 @Component({
   selector: 'app-code-formatter',
   standalone: true,
   imports: [],
   templateUrl: './code-formatter.component.html',
-  styleUrl: './code-formatter.component.scss'
+  styleUrls: ['./code-formatter.component.scss']
 })
-
 export class CodeFormatterComponent {
   @Input() code: string | any;
 
@@ -18,35 +18,44 @@ export class CodeFormatterComponent {
 
   ngAfterViewInit() {
     if (this.editor) {
-      const code = this.code.replace(/\s+/g, ' '); // Remove multiple whitespace characters with a single space
+      let formattedCode = this.code;
 
+      // Attempt to format the code using Prettier
+      try {
+        formattedCode = prettier.format(this.code, {
+          parser: 'babel',
+          plugins: [prettierPlugins.babel],
+          semi: true,
+          singleQuote: true
+        });
+      } catch (error) {
+        // console.warn('Prettier formatting failed:', error);
+        // Proceed with the unformatted code if Prettier throws an error
+      }
+
+      // Initialize ACE editor with either formatted or original code
       const aceEditor = ace.edit(this.editor.nativeElement, {
-        theme: 'ace/theme/monokai',  // Choose a color-coded theme
-        mode: 'ace/mode/javascript', // Enable syntax highlighting for JavaScript
-        value: this.code, // Use the input code
+        theme: 'ace/theme/monokai',
+        mode: 'ace/mode/javascript',
+        value: formattedCode, // Use the formatted or original code
         autoScrollEditorIntoView: true,
-        // maxLines: 20,  // Adjust the editor's height
         minLines: 10,
-        fontSize: 16  // Set font size
+        fontSize: 16
       });
-  
-      // Enable additional settings (no diff or split editor)
+
       aceEditor.setOptions({
-        enableBasicAutocompletion: false,  // Enable autocompletion
-        enableLiveAutocompletion: false,   // Enable live autocompletion
-        enableSnippets: true,             // Enable code snippets
-        showPrintMargin: false,           // Disable print margin (if it was visible)
-        highlightActiveLine: true         // Highlight active line
+        enableBasicAutocompletion: false,
+        enableLiveAutocompletion: false,
+        enableSnippets: true,
+        showPrintMargin: false,
+        highlightActiveLine: true
       });
-  
-      // Align code
-      aceEditor.session.setUseSoftTabs(true); // Enable soft tabs
-      aceEditor.session.setTabSize(4); // Set tab size to 4 spaces
-  
-      // Ensure no diff or split mode is active
-      aceEditor.session.setUseWorker(false);  // Disable workers for highlighting
+
+      aceEditor.session.setUseSoftTabs(true);
+      aceEditor.session.setTabSize(4);
+      aceEditor.session.setUseWorker(false);
     } else {
-      console.error('Editor element not found');
+      // console.error('Editor element not found');
     }
   }
 }
--- src/learning/list/list.component.ts
+++ src/learning/list/list.component.ts
@@ -1,61 +1,101 @@
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
-
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { learningList } from '../learning.constant';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CodeFormatterComponent } from '../code-formatter/code-formatter.component';
 declare const ace: any;
-declare const prettier: any; // Declare the global prettier object
-declare const prettierPlugins: any; // Declare the plugins object from the CDN
 
 @Component({
-  selector: 'app-code-formatter',
+  selector: 'app-list',
   standalone: true,
-  imports: [],
-  templateUrl: './code-formatter.component.html',
-  styleUrls: ['./code-formatter.component.scss']
+  imports: [CommonModule, FormsModule,CodeFormatterComponent],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.scss'
 })
-export class CodeFormatterComponent {
-  @Input() code: string | any;
+export class ListComponent implements OnInit, AfterViewInit {
+  constructor(private router: Router) {
+
+  }
+  
+  listData: any = learningList;
+  selectedValue = null;
+  framPerList = [
+    {
+      name: 'Auto Adjust',
+      value: null
+
+    },
+    {
+      name: '1frame',
+      value:'1'
+
+    },
+    {
+      name: '2frame',
+      value:'2'
+      
+    },
+    {
+      name: '3frame',
+      value:'3'
+    }
+  ];
+  getGridTemplateColumns(): any {
+    return !['', null, undefined, 0, 'null'].includes(this.selectedValue) ? `repeat(${this.selectedValue}, 1fr)` : 'repeat(auto-fit, minmax(500px, 1fr))';
+  }
+  ngOnInit() {}
+
+  navigateToDetail(id: any) {
+    this.router.navigate(['learning/detail', id])
+  }
+
+  copy(text: any,i: any,parentIndex: any,childIndex: any) {
+    this.listData[i].filesList[parentIndex].codeLists[childIndex].isCopied = true;
+    navigator.clipboard.writeText(text);
+    setTimeout(() => {
+      this.listData[i].filesList[parentIndex].codeLists[childIndex].isCopied = false;
+    },4000)
+  }
 
   @ViewChild('editor', { static: false }) editor!: ElementRef;
 
   ngAfterViewInit() {
     if (this.editor) {
-      let formattedCode = this.code;
-
-      // Attempt to format the code using Prettier
-      try {
-        formattedCode = prettier.format(this.code, {
-          parser: 'babel',
-          plugins: [prettierPlugins.babel],
-          semi: true,
-          singleQuote: true
-        });
-      } catch (error) {
-        // console.warn('Prettier formatting failed:', error);
-        // Proceed with the unformatted code if Prettier throws an error
-      }
-
-      // Initialize ACE editor with either formatted or original code
       const aceEditor = ace.edit(this.editor.nativeElement, {
-        theme: 'ace/theme/monokai',
-        mode: 'ace/mode/javascript',
-        value: formattedCode, // Use the formatted or original code
+        theme: 'ace/theme/monokai',  // Choose a color-coded theme
+        mode: 'ace/mode/javascript', // Enable syntax highlighting for JavaScript
+        value: `// Program to reverse a string
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+console.log(reverseString('Hello, World!')); // Output: !dlroW ,olleH`,
         autoScrollEditorIntoView: true,
+        maxLines: 30,  // Adjust the editor's height
         minLines: 10,
-        fontSize: 16
+        fontSize: 16  // Set font size
       });
 
+      // Enable additional settings (no diff or split editor)
       aceEditor.setOptions({
-        enableBasicAutocompletion: false,
-        enableLiveAutocompletion: false,
-        enableSnippets: true,
-        showPrintMargin: false,
-        highlightActiveLine: true
+        enableBasicAutocompletion: false,  // Enable autocompletion
+        enableLiveAutocompletion: false,   // Enable live autocompletion
+        enableSnippets: true,             // Enable code snippets
+        showPrintMargin: false,           // Disable print margin (if it was visible)
+        highlightActiveLine: true         // Highlight active line
       });
 
-      aceEditor.session.setUseSoftTabs(true);
-      aceEditor.session.setTabSize(4);
-      aceEditor.session.setUseWorker(false);
+      // Ensure no diff or split mode is active
+      aceEditor.session.setUseWorker(false);  // Disable workers for highlighting
     } else {
-      // console.error('Editor element not found');
+      console.error('Editor element not found');
     }
   }
+
+
 }
+
+
+
+
